e8f64bd412e07e32dd9fd85e7b0358dbf9627c22
Somnid <omnidestroyer@gmail.com>
2020-07-03 23:28:04 -0700

    adding template for beaker

---
+++ commands/dweb.js
@@ -0,0 +1,23 @@
+import { copyDirectoryRecursiveReplace } from "../utilities/fs-util.js";
+import { substitute } from "../utilities/name-substituter.js";
+import { dirname } from "path";
+
+export async function run(name) {
+	if (!name) {
+		return console.log("Please enter a project name.");
+	}
+	const basedir = dirname(dirname(import.meta.url)).replace("file:///", "");
+	await copyDirectoryRecursiveReplace(
+		`${basedir}/templates/projects/dweb`,
+		`${process.cwd()}/${name}`,
+		/\$(.*?)\$/g,
+		substitute(name)
+	);
+};
+
+export const args = [
+	{
+		"name": "name",
+		"description": "The name of the project"
+	}
+];
--- commands/web.js
+++ commands/web.js
@@ -4,7 +4,7 @@ import { dirname } from "path";
 
 export async function run(name){
     if(!name){
-        return console.log("Please enter a project name");
+        return console.log("Please enter a project name.");
     }
     const basedir = dirname(dirname(import.meta.url)).replace("file:///", "");
     await copyDirectoryRecursiveReplace(
